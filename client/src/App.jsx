14c1e397bc3ea507e5191be81c1bb91d5418c2cb
Aditya-Borhade <adityaborhade793@gmail.com>
2025-06-11 22:20:22 +0530

    Add login and signup page UI for grocery project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Login from './components/Login'
 import Home from './pages/Home'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Footer from './components/Footer'
 import { Toaster } from 'react-hot-toast'
+import { useAppContext } from './context/AppContext'
 
 const App = () => {
-
+   const {showUserlogin} =useAppContext()
   const isSellerPath=useLocation().pathname.includes("seller");
   return (
     <div>
       {isSellerPath ? null : <Navbar/>}
-         
+
+      {showUserlogin ? <Login/> : null}
          <Toaster/>
 
       <div className= {`${isSellerPath ? '' : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
