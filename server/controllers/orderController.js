f0113c9dc701a71bbcdf9de9c7d92959130b8dff
Aditya-Borhade <adityaborhade793@gmail.com>
2025-07-05 09:37:34 +0530

    Implement payment success and failure handling logic

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,6 +1,7 @@
 import Order from '../models/Order.js';
 import Product from '../models/Product.js'
 import stripe from "stripe"
+import User from '../models/User.js';
 // to placeOrder on COD
 
 export const placeOrderCOD = async (req, res) =>{
@@ -99,6 +100,71 @@ export const placeOrderStripe = async (req, res) =>{
         return res.json({success:false, message:error.message});
     }
 }
+  
+
+//stripe webhook to verify payment action 
+export const stripeWebhooks= async(request, response)=>{
+     // stripe gateway initialize
+        const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
+
+        const sig = request.headers["stripe-signature"];
+        let event;
+
+        try{
+            event= stripeInstance.webhooks.constructEvent(
+                request.body,
+                sig,
+                process.env.STRIPE_WEBHOOK_SECRET,
+            )
+        }catch(error){
+             response.status(400).send(`webhook error : ${error.message}`);
+        }
+
+        // handle the event 
+        switch (event.type) {
+            case "payment_intent.succeeded":{
+                const paymentIntent=event.data.object;
+                const paymentIntentId=paymentIntent.id;
+
+                // getting  session metadata 
+                const session = await stripeInstance.checkout.sessions.list({
+                    payment_intent:paymentIntentId,
+                });
+                const {orderId, userId} = session.data[0].metadata;
+                // mark payement as paid 
+                await Order.findByIdAndUpdate(orderId, {isPaid:true});
+                // clear user cart 
+                await User.findByIdAndUpdate(userId, {cartItems:{}});
+                break;
+            }
+             case "payment_intent.payment_failed":{
+                const paymentIntent=event.data.object;
+                const paymentIntentId=paymentIntent.id;
+
+                // getting  session metadata 
+                const session = await stripeInstance.checkout.sessions.list({
+                    payment_intent:paymentIntentId,
+                });
+                const {orderId} = session.data[0].metadata;
+                await Order.findByIdAndDelete(orderId);
+                break;
+
+             }
+                
+       
+        
+            default:
+                console.log(`unhandled event type ${event.type}`);
+                break;
+        }
+        response.json({received:true});
+        
+}
+
+
+
+
+
 
 
 // get orders by users Id
--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ import productRouter from "./routes/productRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import addressRouter from "./routes/addressRoute.js";
 import orderRouter from "./routes/orderRoute.js";
+import { stripeWebhooks } from "./controllers/orderController.js";
 const app=express();
 
 await connectDB();
@@ -20,6 +21,9 @@ await connectCloudinary();
 const port=process.env.PORT || 4000;
 
 const allowedOrigins =['http://localhost:5173']
+
+
+app.post('/stripe', express.raw({type:'application/json'}),stripeWebhooks)
 
 // middleware configuration 
 app.use(cors({ origin: allowedOrigins, credentials: true }));
