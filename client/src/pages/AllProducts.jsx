01a650070489db3d879f787333ac168b47e5860d
Aditya-Borhade <adityaborhade793@gmail.com>
2025-07-09 18:57:24 +0530

    updated allproduct page

--- client/src/pages/AllProducts.jsx
+++ client/src/pages/AllProducts.jsx
@@ -20,7 +20,7 @@ const AllProducts = () => {
 
   return (
     <div className='mt-16 flex flex-col'>
-      <div className='flex flex-col items-end w-max'>
+      <div className='flex flex-col items-end w-max mx-auto sm:mx-0'>
         <p className='text-2xl font-medium uppercase'>All Products</p>
         <div className='w-16 h-0.5 bg-primary rounded-full'></div>
       </div>
